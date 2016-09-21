68240f5c18d05aa00d067eb14b683fd07ddc9d2d
Carl Hopf <git@carlhopf.com>
2016-09-22 00:25:40 +0200

    spec

---
+++ test/subHashPoolMultipleSpec.js
@@ -0,0 +1,97 @@
+const async = require('async');
+const assert = require('assert');
+const q = require('q');
+const config = require('./lib/config');
+const SubHashPool = require('../lib/SubHashPool');
+
+describe('subhashpool multiple', function () {
+	var pool;
+	var channel;
+	var cancels;
+
+	before('init', function (cb) {
+		pool = new SubHashPool({
+			tag: 'test',
+			masters: [
+				{
+					host: config.redisHost,
+					port: config.redisPort,
+				},
+			],
+		});
+
+		pool.init(cb);
+	});
+
+	beforeEach('select random channel', function () {
+		channel = 'chan' + Math.random();
+	});
+
+	beforeEach('subscribe multiple', function (cb) {
+		async.times(
+			Math.floor(Math.random() * 5) + 1,
+			function (i, cb) {
+				pool.subscribe(
+					channel,
+					function (json) {},
+					function (err, cancel) {
+						assert.equal(err, null);
+						cb(null, cancel);
+					});
+			},
+			function (err, _cancels) {
+				cancels = _cancels;
+				cb(err);
+			});
+	});
+
+	it('subscribe 2nd, cancel', function (cb) {
+		var onready2 = false;
+
+		pool.subscribe(
+			channel,
+			function (json) {},
+			function (err, cancel) {
+				// ready callback only once
+				assert.equal(onready2, false);
+				onready2 = true;
+
+				assert.equal(err, null);
+				assert.equal(typeof cancel, 'function');
+
+				cancel(function (err) {
+					cb(err);
+				});
+			});
+	});
+
+	it('subscribe 2nd, cancel instantly', function (cb) {
+		var onready2 = false;
+
+		var cancel2 = pool.subscribe(
+			channel,
+			function (json) {},
+			function (err, cancel) {
+				// assert only called once
+				assert(!onready2);
+				onready2 = true;
+
+				assert.equal(err, 'subscribe cancelled');
+				assert.equal(cancel, undefined);
+			});
+
+		cancel2(function (err) {
+			assert.equal(err, null);
+			assert(onready2);
+			cb(null);
+		});
+
+		assert(!onready2);
+	});
+
+	afterEach('cancel multiple', function (cb) {
+		async.map(cancels, (cancel, cb) => {
+			cancel(cb);
+		}, cb);
+	});
+});
--- test/subHashPoolSpec.js
+++ test/subHashPoolSpec.js
@@ -7,7 +7,7 @@ describe('subhashpool', function () {
 	var pool;
 	var channel;
 
-	before('init pool', function (cb) {
+	before('init', function (cb) {
 		pool = new SubHashPool({
 			tag: 'test',
 			masters: [
@@ -25,59 +25,59 @@ describe('subhashpool', function () {
 		channel = 'chan' + Math.random();
 	});
 
-	it.skip('subscribe and receive json', function () {
-		// TODO
-	});
-
-	it('cancel after ready', function (cb) {
-		var readyCallback = false;
+	it('subscribe, cancel', function (cb) {
+		var onready = false;
 
 		pool.subscribe(
 			channel,
 			function (json) {},
 			function (err, cancel) {
+				// must only run once
+				assert(!onready);
+				onready = true;
+
 				assert.equal(err, null);
 				assert.equal(typeof cancel, 'function');
 
-				cancel(function () {
+				cancel(function (err) {
 					assert(!err);
 
 					// TODO assert client no longer subscribed to channel
-
 					cb(null);
 				});
 			});
 	});
 
-	it('cancel must err ready cb', function (cb) {
-		var readyCallback = false;
+	it('subscribe, instant cancel, err ready cb', function (cb) {
+		var onready = false;
 
 		var cancel = pool.subscribe(
 			channel,
 			function (json) {},
 			function (err, cancel) {
+				// must only run once
+				assert(!onready);
+				onready = true;
+
 				assert.equal(err, 'subscribe cancelled');
 				assert.equal(cancel, undefined);
-
-				// must only run once
-				assert(!readyCallback);
-				readyCallback = true;
 			});
 
 		// instantly cancel
 		cancel(function (err) {
 			assert.equal(err, null);
 
-			// readyCallback must run before this cancel() cb
-			assert(readyCallback);
+			// onready must run before this cancel() cb
+			assert(onready);
+
 			cb(null);
 		});
 
 		// must be invoked in nextTick
-		assert(!readyCallback);
+		assert(!onready);
 	});
 
-	it('cancel1 must err ready cb, cancel2 not', function (cb) {
+	it('subscribe twice, cancel1 must err ready cb, cancel2 not', function (cb) {
 		var readyCb1 = false;
 		var readyCb2 = false;
 
@@ -94,6 +94,7 @@ describe('subhashpool', function () {
 			function (json) {},
 			function (err, cancel) {
 				assert.equal(err, 'subscribe cancelled');
+				assert(!readyCb1);
 				readyCb1 = true;
 			});
 
@@ -102,6 +103,7 @@ describe('subhashpool', function () {
 			function (json) {},
 			function (err, cancel) {
 				assert.equal(err, null);
+				assert(!readyCb2);
 				readyCb2 = true;
 
 				cancel2(function (err) {
@@ -121,65 +123,33 @@ describe('subhashpool', function () {
 		assert(!readyCb2);
 	});
 
-	describe('after first subscribe cb', function () {
-		var cancel1;
-
-		beforeEach('1st subscribe', function (cb) {
-			cancel1 = pool.subscribe(
-				channel,
-				function (json) {},
-				function (err, cancel) {
-					assert.equal(err, null);
-					cb(null);
-				});
-		});
-
-		it('2nd subscribe', function (cb) {
-			pool.subscribe(
-				channel,
-				function (json) {},
-				function (err, cancel) {
-					assert.equal(err, null);
-					assert.equal(typeof cancel, 'function');
-					cb(null);
-				});
-		});
-
-		it('2nd subscribe, cancel instantly', function (cb) {
-			var readyCb2 = false;
-
-			var cancel2 = pool.subscribe(
-				channel,
-				function (json) {},
-				function (err, cancel) {
-					assert.equal(err, 'subscribe cancelled');
-					assert.equal(cancel, undefined);
-
-					// assert only called once
-					assert(!readyCb2);
-					readyCb2 = true;
-				});
-
-			cancel2(function (err) {
-				assert.equal(err, null);
-				assert(readyCb2);
-			});
-
-			assert(!readyCb2);
-		});
-
-		afterEach('cancel1', function (cb) {
-			cancel1(cb);
-		});
-	});
-
-	it.skip('call cancel multiple times', function () {
+	it('cancel multiple times', function () {
 		// TODO behaviour for second call?
 		// - exec 2nd cancel callback after 1st cancel callback (queue)
 		// - never exec callback
+
+		var onready = false;
+
+		pool.subscribe(
+			channel,
+			function (json) {},
+			function (err, cancel) {
+				// must only run once
+				assert(!onready);
+				onready = true;
+
+				cancel(function (err) {
+					assert(!err);
+					cb(null);
+				});
+
+				cancel(function (err) {
+					assert(false);
+				});
+			});
 	});
 
-	afterEach('stop pub json', function () {
-
+	it.skip('subscribe and receive json', function () {
+		// TODO
 	});
 });

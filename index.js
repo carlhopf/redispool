cc37c7ae235244f11117831d85b54ccabb4a299b
Carl Hopf <git@carlhopf.com>
2016-09-22 16:11:58 +0200

    1.1.1

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 exports.MasterSlavePool = require('./lib/MasterSlavePool');
 exports.MasterSlaveHashPool = require('./lib/MasterSlaveHashPool');
+exports.SubHashPool = require('./lib/SubHashPool');
+exports.PubHashPool = require('./lib/PubHashPool');
